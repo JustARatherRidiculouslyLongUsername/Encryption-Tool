0bb7ee466bb5279fa66e8229d392a82b7be92428
JustARatherRidiculouslyLongUsername <37623357+JustARatherRidiculouslyLongUsername@users.noreply.github.com>
2018-03-21 17:05:36 +0400

    Adding the file

---
+++ Encryptor.py
@@ -0,0 +1,13 @@
+def encrypt(string, key):
+	encrypted_str = ''
+	for i in string.encode():
+		encrypted_str += chr(i + key)
+		
+	return encrypted_str
+    
+def decrypt(encrypted_str, key):
+	decrypted_str = ''
+	for i in encrypted_str.encode():
+		decrypted_str += chr(i - key)
+		
+	return decrypted_str
