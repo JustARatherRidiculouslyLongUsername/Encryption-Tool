62642103e1e5d8abc5ca7605970ba791a47c6323
JustARatherRidiculouslyLongUsername <37623357+JustARatherRidiculouslyLongUsername@users.noreply.github.com>
2018-03-21 17:08:19 +0400

    Update Encryptor.py

--- Encryptor.py
+++ Encryptor.py
@@ -1,13 +1,11 @@
 def encrypt(string, key):
 	encrypted_str = ''
 	for i in string.encode():
-		encrypted_str += chr(i + key)
-		
+		encrypted_str += chr(i + key)		
 	return encrypted_str
     
 def decrypt(encrypted_str, key):
 	decrypted_str = ''
 	for i in encrypted_str.encode():
-		decrypted_str += chr(i - key)
-		
+		decrypted_str += chr(i - key)	
 	return decrypted_str
